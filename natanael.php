61401ff70a40902ffb94515587e54bcb65241368
NatanaelWT <natanaelhdxd25@gmail.com>
2024-04-21 20:27:23 +0700

    progress natan

--- natanael.php
+++ natanael.php
@@ -9,7 +9,97 @@
 </head>
 
 <body>
+    <header class="mt-3 mx-6">
+        <div class="bg-white text-black py-4 border rounded-3xl border-slate-200 shadow-md">
+            <div class="grid grid-cols-4 text-center font-semibold text-lg">
+                <a href="#">
+                    <div class="hover:text-blue-800">
+                        Profil
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="hover:text-blue-800">
+                        About Me
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="hover:text-blue-800">
+                        Experience
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="hover:text-blue-800">
+                        Contact
+                    </div>
+                </a>
+            </div>
+        </div>
+    </header>
 
+    <section>
+        <div class="grid grid-cols-6 m-4 border rounded-3xl border-slate-200 shadow-md gap-4 p-10">
+            <div class="col-span-6 text-3xl font-bold text-center mb-8">
+                <h1>PROFIL</h1>
+            </div>
+            <div class="col-span-2">
+                <img src="img/Natan.jpg" class="rounded-full w-96 h-w-96 mx-auto" alt="">
+
+            </div>
+            <div class="col-span-3">
+                <div class="grid grid-cols-6 text-left text-2xl font-bold">
+                    <div>
+                        <h2>Nama</h2>
+                        <h2>NIM</h2>
+                        <h2>Kampus</h2>
+                    </div>
+                    <div class="col-span-5">
+                        <h2>: Natanael Wijaya Tiono</h2>
+                        <h2>: 1203220140</h2>
+                        <h2>: Telkom University Surabaya</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="">
+
+            </div>
+        </div>
+        <div class="grid grid-cols-3 m-4 border rounded-3xl border-slate-200 shadow-md gap-4 p-10">
+            <div class="col-span-3 text-3xl font-bold text-center mb-8">
+                <h1>ABOUT ME</h1>
+            </div>
+            <div class="col-span-3">
+                <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur vitae ipsa mollitia iusto fugiat, et corrupti cumque excepturi sunt, ratione assumenda illum iste quam! Itaque dolor illo voluptatem labore deleniti enim inventore nesciunt eius odit, et tempore, eveniet distinctio impedit atque? Ullam voluptatibus commodi corporis illo, iste in quo? Dolor illum distinctio earum aut laborum harum? Impedit nostrum, similique iste illo optio accusantium exercitationem reiciendis pariatur doloremque molestiae voluptatibus iure obcaecati quod. Harum, eligendi placeat. Doloribus, voluptatum at! Expedita praesentium ratione atque suscipit inventore neque distinctio voluptates totam natus ullam! Libero dolore accusamus at quos, omnis repellat id consequatur delectus repellendus magnam animi, perspiciatis recusandae, earum ratione ullam aspernatur blanditiis dolorem vero deleniti illum? Quidem, ipsum voluptatum? Omnis amet repudiandae facilis accusantium aspernatur sequi excepturi vero nulla, impedit perferendis, porro nihil harum ipsa ipsum fugiat blanditiis architecto soluta perspiciatis, error quasi delectus eos! Inventore iste vero saepe molestias nulla ipsam voluptates facilis culpa perspiciatis rem, sapiente recusandae. Cupiditate sunt, minima dolore dolorum mollitia iusto voluptas ducimus explicabo, assumenda dolorem perspiciatis ad iure veniam eum nam quaerat, adipisci ratione facilis! Quae consequatur quos incidunt iste voluptatibus assumenda totam unde nisi ad vitae illo, voluptas maxime quaerat magnam consequuntur cum architecto ratione quod officiis quasi. Laboriosam, nostrum nihil! Laudantium, cumque ducimus animi mollitia rerum deserunt adipisci, alias accusamus earum nesciunt amet. Et, rem blanditiis architecto dolor reprehenderit hic libero placeat repudiandae! Eum asperiores nemo necessitatibus quisquam suscipit error quod tempora expedita corrupti! Magni fugiat quos, neque eveniet in tempora rem, nesciunt debitis, voluptatem maxime voluptas non cupiditate tenetur. Ipsum laboriosam rerum nesciunt earum delectus et aut consequuntur itaque ut unde, inventore expedita eos enim, dolores iste? Aliquid dolore aperiam unde laboriosam excepturi laborum maxime quis eius in, perspiciatis fugiat non consequatur inventore eveniet beatae, dicta quos aspernatur est quam dolorem porro error.</h1>
+            </div>
+        </div>
+        <div class="grid grid-cols-3 m-4 border rounded-3xl border-slate-200 shadow-md gap-4 p-10">
+            <div class="col-span-3 text-3xl font-bold text-center mb-8">
+                <h1>EXPERIENCE</h1>
+            </div>
+            <div class="">
+                <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id magni, exercitationem, minus eveniet reiciendis ratione fuga voluptatum eligendi similique et cum, aut sit molestias itaque cupiditate libero adipisci voluptates neque.</h1>
+            </div>
+            <div class="">
+                <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id magni, exercitationem, minus eveniet reiciendis ratione fuga voluptatum eligendi similique et cum, aut sit molestias itaque cupiditate libero adipisci voluptates neque.</h1>
+            </div>
+            <div class="">
+                <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id magni, exercitationem, minus eveniet reiciendis ratione fuga voluptatum eligendi similique et cum, aut sit molestias itaque cupiditate libero adipisci voluptates neque.</h1>
+            </div>
+        </div>
+        <div class="grid grid-cols-3 m-4 border rounded-3xl border-slate-200 shadow-md gap-4 p-10">
+            <div class="col-span-3 text-3xl font-bold text-center mb-8">
+                <h1>CONTACT</h1>
+            </div>
+            <div class="">
+                <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id magni, exercitationem, minus eveniet reiciendis ratione fuga voluptatum eligendi similique et cum, aut sit molestias itaque cupiditate libero adipisci voluptates neque.</h1>
+            </div>
+            <div class="">
+                <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id magni, exercitationem, minus eveniet reiciendis ratione fuga voluptatum eligendi similique et cum, aut sit molestias itaque cupiditate libero adipisci voluptates neque.</h1>
+            </div>
+            <div class="">
+                <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id magni, exercitationem, minus eveniet reiciendis ratione fuga voluptatum eligendi similique et cum, aut sit molestias itaque cupiditate libero adipisci voluptates neque.</h1>
+            </div>
+        </div>
+    </section>
 </body>
 
 </html>
